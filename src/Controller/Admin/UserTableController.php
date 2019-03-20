1f62aa339556bdfad012058fdfa35df4e72ee093
Tim <47831783+TimBrich@users.noreply.github.com>
2019-03-21 00:31:28 +0300

    UserTable

---
+++ src/Controller/Admin/UserTableController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserTableController extends AbstractController
+{
+    /**
+     * @Route("/usertable",name="userTableController")
+     */
+    public function home(UserRepository $userRepository)
+    {
+        
+    }
+}
