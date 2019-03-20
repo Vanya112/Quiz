b11e94f2ec6f28cabc9c988f03542bc1e2c5ade4
Tim <47831783+TimBrich@users.noreply.github.com>
2019-03-21 00:30:00 +0300

    GetRequestController_realize

--- src/Controller/Admin/GETRequestController.php
+++ src/Controller/Admin/GETRequestController.php
@@ -19,6 +19,19 @@ class GETRequestController extends AbstractController
      */
     public function getClass()
     {
-        
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository(User::class);
+        $delete = new deleteUser();
+        $filter = new filterUser();
+        $edit = new editUser();
+        $services = array("delete");
+        $request = Request::createFromGlobals();
+        $type = $request->query->get('type');
+        if (in_array($type, $services))
+        {
+            $response = $$type -> editData($request->query->get('data'), $repository, $em);
+            return $response;
+        }
+        return new Response('<html><body>1234</body></html>');
     }
 }
