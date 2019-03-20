5bd9aad53fc18547f837b6170fea2cf8147cc01f
Ilya Belevich <ilya.belevich@mail.ru>
2019-03-21 00:48:38 +0300

    Changing registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,30 +16,5 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/register", name="new_user")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
-    {
-        $user = new User();
-        $form = $this->createForm( RegistrationForm::class, $user);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-            $user->setPassword($password);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            try {
-                $entityManager->flush();
-            }
-            catch (UniqueConstraintViolationException $e)
-            {
-                return $this->redirectToRoute('fail', array('type'=>"registration",
-                    'text' => "This name or email is already taken, try to enter differently"
-                ));
-            };
-            return $this->redirectToRoute('success',array('type'=>"register"),301);
-        }
-        return $this->render(
-            'security/register.html.twig',
-            array('form' => $form->createView())
-        );
-    }
+    
 }
