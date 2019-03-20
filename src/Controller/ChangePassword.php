e5879111985975bbce158eece42ba0b872753492
Ilya Belevich <ilya.belevich@mail.ru>
2019-03-21 00:53:41 +0300

    Password adding controller

--- src/Controller/ChangePassword.php
+++ src/Controller/ChangePassword.php
@@ -5,5 +5,10 @@ namespace App\Controller\Security;
 
 class ChangePassword
 {
-    
+    /**
+     * @SecurityAssert\UserPassword(
+     *     message = "Wrong value for your current password"
+     * )
+     */
+    protected $oldPassword;
 }
